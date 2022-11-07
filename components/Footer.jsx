91ca661f352b9c055724e448fdaac96c285a7f4f
Tobbiloba <salauoluwatobiloba0@gmail.com>
2022-11-07 14:17:21 +0100

    my ecommerce website

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
+
+
+export const Footer = () => {
+  return (
+    <div className='footer-container'> 
+    <p>2022 TEE NIKE Sneakers All rights reserved</p>
+    <p className='icons'>
+      <AiFillInstagram />
+      <AiOutlineTwitter />
+    </p>
+    </div>
+  )
+}
